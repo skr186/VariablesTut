27f7a104ba237c94afd39af4cbb7627eda7dd85d
skr186 <sakar1994@gmail.com>
2020-01-29 00:15:10 +0545

    This a New Branch

--- Animal.java
+++ Animal.java
@@ -34,24 +34,24 @@ public class Animal {
 		
 		System.out.println("==========================================================");
 		
-		String name1= "Tiger"; //Local Variable
-		AnimalDetail ad1 = new AnimalDetail(4,2,1); //From Parameterized Constructor in class AnimalDetail
-		speed = 60;
-		AnimalDetail.Location = "Rainforest";
-		
-		System.out.println("Kingdom: " + AnimalDetail.Kingdom + " --> [Static/Class Variable]");
-		System.out.println("Name: " + name1 + " --> [Local Variable]");
-		System.out.println("Running Speed: " + speed + " Km/h --> [Instance Variable]");
-		
-		System.out.println("==========================================================");
-		
-		System.out.println("No. of Legs : " + ad1.leg + " ---|");
-		System.out.println("No. of Eyes : " + ad1.eye + " ---|---> [From Parameterized Constructor]");
-		System.out.println("No. of Tails: " + ad1.tail+" ---|");
-		
-		System.out.println("==========================================================");
-
-		System.out.println("Current Location: " + AnimalDetail.Location + " ---> [Static variable/ Only one value in whole program]");
-		System.out.println("Current Location: " + ad1.Location + " ---> [Static variable/ Only one value in whole program]");
+//		String name1= "Tiger"; //Local Variable
+//		AnimalDetail ad1 = new AnimalDetail(4,2,1); //From Parameterized Constructor in class AnimalDetail
+//		speed = 60;
+//		AnimalDetail.Location = "Rainforest";
+//		
+//		System.out.println("Kingdom: " + AnimalDetail.Kingdom + " --> [Static/Class Variable]");
+//		System.out.println("Name: " + name1 + " --> [Local Variable]");
+//		System.out.println("Running Speed: " + speed + " Km/h --> [Instance Variable]");
+//		
+//		System.out.println("==========================================================");
+//		
+//		System.out.println("No. of Legs : " + ad1.leg + " ---|");
+//		System.out.println("No. of Eyes : " + ad1.eye + " ---|---> [From Parameterized Constructor]");
+//		System.out.println("No. of Tails: " + ad1.tail+" ---|");
+//		
+//		System.out.println("==========================================================");
+//
+//		System.out.println("Current Location: " + AnimalDetail.Location + " ---> [Static variable/ Only one value in whole program]");
+//		System.out.println("Current Location: " + ad1.Location + " ---> [Static variable/ Only one value in whole program]");
 	}
 }
